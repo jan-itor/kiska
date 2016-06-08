fa4d63d394e2e78068d0887d49d15cd46f8c0891
Andrey Masyuk <andrey06945@gmail.com>
2016-06-08 12:27:12 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_kiska',
+    'username' => 'root',
+    'password' => '123',
+    'charset' => 'utf8',
+];
---
+++ views/site/index.php
@@ -0,0 +1,52 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Algoritm';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Cтудия web-разработки</h1>
+
+        <p class="lead">Разрабатываем и продвигаем</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://web-algoritm.su/" target="_blank">Начни с нами</a></p>
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-3">
+                <h2>Лояльная ценовая политика</h2>
+
+                <p>Разработка веб сайтов любого типа и предназначения: от небольшой визитки до крупного портала. Только функциональные и эффективные решения, способные оперативно справиться с поставленной задачей.</p>
+
+                <p><a class="btn btn-default" href="http://web-algoritm.su/sozdanie-saytov/" target="_blank">Разработка сайтов &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h2>Ставка на результат</h2>
+
+                <p>SEO продвижение сайта от нашей компании – это гарантированное привлечение потенциальных клиентов без каких-либо рисков попасть под фильтры поисковых систем.</p>
+
+                <p><a class="btn btn-default" href="http://web-algoritm.su/prodvizhenie-saytov/" target="_blank">Продвижение сайтов &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h2>Надежность</h2>
+
+                <p>Контекстная реклама в грамотных руках – это самый быстрый и эффективный способ достучаться до вашей целевой аудитории. Профессиональная настройка Яндекс Директ и Google AdWords.</p>
+
+                <p><a class="btn btn-default" href="http://web-algoritm.su/kontekstnaya-reklama/" target="_blank">Контекстная реклама &raquo;</a></p>
+            </div>
+            <div class="col-lg-3">
+                <h2>Полный комплекс услуг</h2>
+
+                <p>Техническая и информационная поддержка сайтов от «Алгоритм» - это комплексная  работа, способная лишить вас проблем, связанных с хостингом, доменом, некачественным контентом, неисправными модулями, вирусами и. т. д.</p>
+
+                <p><a class="btn btn-default" href="http://web-algoritm.su/dopolnitelnye-uslugi/" target="_blank">Поддержка сайтов &raquo;</a></p>
+            </div>
+
+        </div>
+
+    </div>
+</div>
